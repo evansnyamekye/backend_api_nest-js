f11b64f31b938c50058b79d071e0fe9552908e4d
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-04-12 23:57:11 +0000

    Add controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Import the module
+ ConfigMoudle.forRoot(), 
+ TypeOrmModule.forRoot({
+  
+ })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
