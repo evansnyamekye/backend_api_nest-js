2017e3a3dc1ebce6061504ee2dd1f225c9f4b2f1
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-06 23:36:12 +0000

    Revert "Revert "Revert "Implement Controllers for Application - feature/controllers🎮 """

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    // Import the module
- ConfigModule.forRoot(), 
- TypeOrmModule.forRootRootAync (), 
- TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
- }),
- 
- })
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
