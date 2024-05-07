d74818c3df9b7ab818f3f5b8e2dffb801a0686c0
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-07 11:28:47 +0000

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 /*all the import are based on the configurationService. 
 */
 
-@Module({
+ @Module({
   imports: [
-    // Import the module
- ConfigModule.forRoot(), 
- TypeOrmModule.forRootRootAync (), 
- TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
- }),
- 
- })
+    // Import the module two modules and configure typeOrem. 
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        post: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')], 
+        synchronize: true, 
+      }),
+    }), 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {}import { join } from 'path';
+
