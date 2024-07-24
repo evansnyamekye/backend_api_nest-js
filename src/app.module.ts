790dced1a63f3de0ab6b5aadacedaf7545684b91
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-07-24 14:54:16 +0000

    Revert "Feature/controller btn"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
-/*all the import are based on the configurationService. 
-*/
-
- @Module({
-  imports: [
-    // Import the module two modules and configure typeOrem. 
-    ConfigModule.forRoot(), 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService], 
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        post: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(), 'dist/**/*.entity.js')], 
-        synchronize: true, 
-      }),
-    }), 
-  ],
-   controllers: [AppController], 
-   providers: [AppService], 
- })
-export class AppModule {}import { join } from 'path';
-
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
